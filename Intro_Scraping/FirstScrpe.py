0de8a1d1cc20d9080bebd1c63d5ffb3b817c0b37
Daniel Mints <67702241+DanMint@users.noreply.github.com>
2022-12-19 19:41:54 -0500

    Update FirstScrpe.py

--- Intro_Scraping/FirstScrpe.py
+++ Intro_Scraping/FirstScrpe.py
@@ -1,8 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
+# This means go to this website
 page_to_scrape = requests.get("https://quotes.toscrape.com/")
+# Here we use soup to parse the website 
 soup = BeautifulSoup(page_to_scrape.text, "html.parser")
+# He we scrape
 quotes = soup.findAll("span", attrs={"class": "text"})
 authors = soup.findAll("small", attrs={"class": "authors"})
 
